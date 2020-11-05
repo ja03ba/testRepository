8e0fbb72304008f7164c8b643b601618e77afffe
ja03ba <49076529+ja03ba@users.noreply.github.com>
2020-11-05 14:23:56 +0100

    TestFile.cs added to project

---
+++ sandboxLinq/sandboxLinq/TestFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sandboxLinq
+{
+    class TestFile
+    {
+        public void NewMethod()
+        {
+            Console.WriteLine("New method");
+        }
+    }
+}
