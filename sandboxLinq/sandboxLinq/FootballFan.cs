d780d8f6f0e79db62e9a052d3d09dca6ab5acac2
ja03ba <49076529+ja03ba@users.noreply.github.com>
2020-11-05 14:18:44 +0100

    Add files via upload

---
+++ sandboxLinq/sandboxLinq/FootballFan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sandboxLinq
+{
+    class FootballFan
+    {
+        public int FootballFanSinceYear { get; set; }
+        public FootballClub FavoriteClub { get; set; }
+        public int VisitsThisSeason { get; set; }
+        public bool IsUltraFan { get; set; }
+        public SeasonTicket SeasonTicket { get; set; }
+        public enum FootballClub
+        {
+            Slavia, Sparta, Plzen, Olomouc, Jablonec, Banik, Karvina, Bohemians
+        }
+    }
+}
---
+++ sandboxLinq/sandboxLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace sandboxLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FilterData filterData = new FilterData(1000);
+
+            filterData.GetStudentsWithName("Bartoň");
+            //filterData.CreateStudentsGroupByClub();
+            //filterData.GetStudentsWithDiscount();
+            //filterData.GetVIPSeasonTicketFans();
+            //filterData.GetTeamWithMostUltraFans();
+        }
+    }
+}
